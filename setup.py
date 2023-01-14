3c43531f50ae106f36de8bc4d50b6c5efa2e960c
johncaged <573697439@qq.com>
2023-01-15 00:34:25 +0800

    REF: update setup config.

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     url='https://github.com/johncaged/TorchSlime',
     author='Zikang Liu',
     author_email='liuzikang0625@gmail.com',
+    license='MIT License',
     description='TorchSlime is a PyTorch-based framework that helps you easily build your DL projects with pre-defined, highly extensible pipelines and other utils.',
     long_description=open(README, encoding='utf-8').read(),
     long_description_content_type='text/markdown'
