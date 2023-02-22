03b714cc349fd669b5e119168cae197893b20924
johncaged <573697439@qq.com>
2023-02-22 17:09:34 +0800

    FEA: plugin interface definition

--- torchslime/core/plugin.py
+++ torchslime/core/plugin.py
@@ -3,7 +3,32 @@ from torchslime.util import BaseList
 
 class Plugin:
     
-    pass
+    def __init__(self) -> None:
+        pass
+    
+    def before_build_all(self, ctx):
+        pass
+    
+    def after_build_all(self, ctx):
+        pass
+    
+    def before_build_train(self, ctx):
+        pass
+    
+    def after_build_train(self, ctx):
+        pass
+    
+    def before_build_eval(self, ctx):
+        pass
+    
+    def after_build_eval(self, ctx):
+        pass
+    
+    def before_build_predict(self, ctx):
+        pass
+    
+    def after_build_predict(self, ctx):
+        pass
 
 
 class PluginContainer(Plugin, BaseList):
