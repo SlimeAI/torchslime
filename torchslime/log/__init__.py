6766f84564209cb4ef455692eb3f6303e97390d0
johncaged <573697439@qq.com>
2023-03-15 19:18:40 +0800

    FIX: maximum recursion depth exceeded error.

--- torchslime/log/__init__.py
+++ torchslime/log/__init__.py
@@ -164,9 +164,18 @@ class LoggerProxy:
         self._logger: Union[Logger, DistributedLogger] = Logger()
     
     def __getattr__(self, __name: str) -> Any:
+        if __name == '_logger':
+            return super().__getattr__(__name)
         return self._logger.__getattr__(__name)
     
+    def __getattribute__(self, __name: str) -> Any:
+        if __name == '_logger':
+            return super().__getattribute__(__name)
+        return self._logger.__getattribute__(__name)
+    
     def __setattr__(self, __name: str, __value: Any) -> None:
+        if __name == '_logger':
+            return super().__setattr__(__name, __value)
         return self._logger.__setattr__(__name, __value)
     
     def __getitem__(self, __i) -> Any:
